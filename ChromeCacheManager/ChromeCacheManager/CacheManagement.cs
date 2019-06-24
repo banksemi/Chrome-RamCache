d1de59fc3f1fb4177ef4a4d2328569ee00cc6521
Seunghwa <banksemi@naver.com>
2019-06-24 18:06:20 +0900

    Implement Simple Main Function
    If  temporary folder is X:\Temp
    The program is working.

---
+++ ChromeCacheManager/ChromeCacheManager/CacheManagement.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+namespace ChromeCacheManager
+{
+    public static class CacheManagement
+    {
+        private static string RamDisk_Temp = @"X:\TEMP\";
+        private static void StartTemp(string ori)
+        {
+            string temp = ori.Replace("%", "-");
+            temp = temp.Replace("\\", "_");
+            temp = RamDisk_Temp + temp;
+            Directory.CreateDirectory(temp);
+            Command.Execute("rd /s /q " + "\"" + ori + '"');
+            Command.Execute("mklink /j " + "\"" + ori + '"' + " \"" + temp + "\"");
+        }
+        public static void Start()
+        {
+            StartTemp(@"%LocalAppData%\Google\Chrome\User Data\Default\Cache");
+            StartTemp(@"%LocalAppData%\Google\Chrome\User Data\Default\Code Cache");
+            StartTemp(@"%LocalAppData%\Google\Chrome\User Data\Default\GPUCache");
+            StartTemp(@"%LocalAppData%\Google\Chrome\User Data\Default\File System");
+            StartTemp(@"%LocalAppData%\Google\Chrome\User Data\ShaderCache");
+        }
+    }
+}
--- ChromeCacheManager/ChromeCacheManager/Program.cs
+++ ChromeCacheManager/ChromeCacheManager/Program.cs
@@ -12,11 +12,18 @@ namespace ChromeCacheManager
         /// 해당 애플리케이션의 주 진입점입니다.
         /// </summary>
         [STAThread]
-        static void Main()
+        static void Main(string[] args)
         {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            if (args.Contains("/auto"))
+            {
+                CacheManagement.Start();
+            }
+            else
+            {
+                Application.EnableVisualStyles();
+                Application.SetCompatibleTextRenderingDefault(false);
+                Application.Run(new Form1());
+            }
         }
     }
 }
