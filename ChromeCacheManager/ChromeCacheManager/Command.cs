507dcb25958e3890577d24a2335798b6d3a133fa
Seunghwa <banksemi@naver.com>
2019-06-24 17:40:56 +0900

    Implement CMD Utility Class

---
+++ ChromeCacheManager/ChromeCacheManager/Command.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Diagnostics;
+
+namespace ChromeCacheManager
+{
+    public static class Command
+    {
+        public static string Execute(string line)
+        {
+            ProcessStartInfo info = new ProcessStartInfo();
+            info.CreateNoWindow = true;
+            info.FileName = "cmd";
+            info.RedirectStandardOutput = true;
+            info.RedirectStandardInput = true;
+            info.RedirectStandardError = true;
+            info.UseShellExecute = false;
+
+            Process process = new Process();
+            process.StartInfo = info;
+            process.Start();
+
+            process.StandardInput.WriteLine(line);
+            process.StandardInput.Close();
+
+
+            process.WaitForExit();
+
+            string result = process.StandardOutput.ReadToEnd();
+            process.Close();
+            return result;
+        }
+    }
+}
